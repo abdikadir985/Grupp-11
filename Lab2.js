3aa9b39b769ae11e05fe2df4e20fda5813ce8b24
oehqoehfo <hosap4ik@gmail.com>
2020-03-18 13:09:28 +0100

    Update Lab2.js

--- Lab2.js
+++ Lab2.js
@@ -1,19 +1,38 @@
 //Lab2.js
+//html elementer
 let option = document.getElementById("cbofabrikat");
-let textRuta = document.querySelectorAll("[type=\"text\"");
+let textRuta = document.querySelectorAll("[type=\"text\"]");
+let currentText = null;
+let checkBox = document.getElementById("cbokommentar");
+let textArea = document.getElementById("txtkommentar");
+//validera formuläret
 function validateForm() {
-    console.log("validera");
     try{
+        //om man har inte valt någonting i listan
         if(option.value=="saknas"){
-            throw "välj en annan rad i select-listan";
+            throw new Error ("välj en annan rad i select-listan");
         }
+        //loopa alla textrutor
+        for(let i=0;i<textRuta.length;i++){
+            currentText = textRuta.item(i);
+            //om någon textrutan är tom skicka error
+            if(currentText.value.length==""){
+                throw new Error ("Fyll gärna textrutan " + i+1);
+            }
+        }
+        if(checkBox.checked){
+            if(textArea.innerHTML.value<5){
+                throw new Error ("Kommentar måste vara längre än 5 tecken");
+            }
+        }
+       //om finns error 
     }catch(error){
         alert(error);
         return false;
     }
     return true;
 }
+//reset formuläret
 function resetForm() {
-    console.log("reset");
     return false;
 }
