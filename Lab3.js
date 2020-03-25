7c7324aebb131c0b9580f8f9318d8c8d1d13dd77
taskmaster <60099108+abdikadir985@users.noreply.github.com>
2020-03-25 14:39:11 +0100

    Update Lab3.js

--- Lab3.js
+++ Lab3.js
@@ -1,7 +1,12 @@
 //Lab3.js
 window.addEventListener("load", prepareNestedUnorderedList);
 
-function prepareNestedUnorderedList() {}
+function prepareNestedUnorderedList() {
+
+let links = document.querySelectorAll('.glyphicon.glyphicon-minus');
+
+
+}
 
 function changeUnorderedListElement( inEvent ) {
     if(this.classList.contains("glyphicon-minus")){
